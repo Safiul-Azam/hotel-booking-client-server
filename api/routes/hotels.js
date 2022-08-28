4d0e675a26a34f05d5731d9357690dfa58a81365
Safiul-Azam <safiulazamriad@gmail.com>
2022-08-28 19:14:26 +0600

    featured show through dynamic find hotel by city

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import { countByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 const router = express.Router()
 
@@ -10,8 +10,9 @@ router.put('/:id', verifyAdmin, updateHotel)
 //delete
 router.delete('/:id', verifyAdmin, deleteHotel)
 //get
-router.get('/:id', getHotel)
+router.get('/find/:id', getHotel)
 //get all
 router.get('/', getHotels)
+router.get("/countByCity", countByCity)
 
 export default router
