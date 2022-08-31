45c9f38cb253db90d6dbe074b5c91916f0d3af8d
Safiul-Azam <safiulazamriad@gmail.com>
2022-08-31 19:45:45 +0600

    reserve room done with selected room and check available

--- api/controllers/room.js
+++ api/controllers/room.js
@@ -26,6 +26,18 @@ export const updateRoom = async (req, res, next) => {
         res.status(500).json(err)
     }
 }
+export const updateRoomAvailability = async (req, res, next) => {
+    try {
+        const updateRoom = await Room.updateOne({"roomNumbers._id":req.params.id},{
+            $push: {
+                "roomNumbers.$.unavailableDates": req.body.dates
+            }
+        })
+        res.status(200).json(updateRoom)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
 export const deleteRoom = async (req, res, next) => {
     const hotelId = req.params.hotelId
     try {
--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -1,13 +1,55 @@
-import React from 'react';
+import './Reserve.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 import useFetch from '../../hooks/useFetch';
+import { useState } from 'react';
+import { useContext } from 'react';
+import { SearchContext } from '../../context/SearchContext';
+import axios from 'axios';
 
 const Reserve = ({ setOpen, hotelId }) => {
-    
+    const [selectedRooms, setSelectedRooms] = useState([])
     const { data, loading, error } = useFetch(`http://localhost:5000/api/hotels/rooms/${hotelId}`)
+    const { dates } = useContext(SearchContext)
 
-    console.log(data);
+    const getDatesInRange = (startDate, endDate) => {
+        const start = new Date(startDate)
+        const end = new Date(endDate)
+        const date = new Date(start.getTime())
+
+        const dates = []
+
+        while (date <= end) {
+            dates.push(new Date(date).getTime())
+            date.setDate(date.getDate() + 1)
+        }
+        return dates
+    }
+    const allDates = getDatesInRange(dates[0]?.startDate, dates[0]?.endDate);
+    const isAvailable = (roomNumber) => {
+        const isFound = roomNumber.unavailableDates.some((date) =>
+          allDates.includes(new Date(date).getTime())
+        );
+    
+        return !isFound;
+      };
+
+    // console.log(data);
+    const handleSelect = e => {
+        const checked = e.target.checked
+        const value = e.target.value
+        setSelectedRooms(checked ? [...selectedRooms, value] : selectedRooms.filter(item => item !== value))
+    }
+
+    const handleClick = async ()=>{
+        try {
+            await Promise.all(selectedRooms.map(roomId => {
+                const res = axios.put(`http://localhost:5000/api/rooms/availability/${roomId}`, {dates:allDates})
+                return res.data
+            }))
+        } catch (err) {}
+    }
+    console.log(selectedRooms);
     return (
         <div className="reserve">
             <div className="rContainer">
@@ -25,14 +67,17 @@ const Reserve = ({ setOpen, hotelId }) => {
                             <div className="rMax">Max people: {item.maxPeople}</div>
                             <div className="rMax">Max people: {item.price}</div>
                             {item.roomNumbers.map(roomNumber => (
-                                <div>
+                                <div key={roomNumber._id}>
                                     <label>{roomNumber.number}</label>
-                                    <input type="checkbox" value={roomNumber.unavailableDates} />
+                                    <input disabled={!isAvailable(roomNumber)} type="checkbox" value={roomNumber._id} onChange={handleSelect} />
                                 </div>
                             ))}
                         </div>
                     </div>
                 ))}
+                <button onClick={handleClick} className="rButton">
+                    Reserve Now!
+                </button>
             </div>
         </div>
     );
